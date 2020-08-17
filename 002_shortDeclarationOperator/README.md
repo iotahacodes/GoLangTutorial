990bd91391481d2733453314cb2dc30a6a168a49
Taha Farahani <iotahacodes@gmail.com>
2020-08-17 14:48:57 +0430

    Short Declaration Operator

---
+++ 002_shortDeclarationOperator/README.md
@@ -0,0 +1,3 @@
+# 002_shortDeclarationOperator
+
+How to declare a variable and assign or change it.
---
+++ 002_shortDeclarationOperator/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 85
+	fmt.Println(a)
+
+	a = 69
+	fmt.Println(a)
+
+	b := 85 + 69
+	fmt.Println(b)
+
+	c := "Broooo..."
+	fmt.Println(c)
+}
