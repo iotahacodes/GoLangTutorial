d8d7a3ecfef5d0779bd7523df06db65e9995d5ab
Taha Farahani <iotahacodes@gmail.com>
2020-08-19 12:29:36 +0430

    maps

--- 012_maps/main.go
+++ 012_maps/main.go
@@ -1,4 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
+	m := make(map[string]int)
+	m["k1"] = 85
+	m["k2"] = 69
+
+	fmt.Println("map:\t", m)
+	fmt.Println("len:\t", len(m))
+
+	delete(m, "k2")
+	fmt.Println("map:\t", m)
+
+	tmp := map[string]int{"jafar": 01, "ghasem": 02}
+	fmt.Println(tmp)
 }
