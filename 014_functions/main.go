6e60b9a459193bf855c77565bb0f027a63bb72f8
Taha Farahani <iotahacodes@gmail.com>
2020-08-19 16:13:07 +0430

    multiple returns

--- 014_functions/main.go
+++ 014_functions/main.go
@@ -2,13 +2,14 @@ package main
 
 import "fmt"
 
-func plus(a int, b int) int {
-	return a + b
+func mathBazi(a int, b int) (int, int) {
+	return a + b, a * b
 }
 
 func main() {
 	a := 85
 	b := 69
 
-	fmt.Println(plus(a, b))
+	add, mult := mathBazi(a, b)
+	fmt.Println(add, mult)
 }
