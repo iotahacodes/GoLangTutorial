10aa48dcb52bf9dcb1df54a594c12c0ffa27c348
Taha Farahani <iotahacodes@gmail.com>
2020-08-19 11:56:50 +0430

    defer stack

--- 011_defers/main.go
+++ 011_defers/main.go
@@ -3,9 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	defer fmt.Println("Done, function returned")
+	defer fmt.Println("\nDone, function returned")
 
 	for i := 1; i <= 5; i++ {
 		fmt.Println(i)
 	}
+
+	fmt.Println()
+	lastInFirstOut()
+}
+
+func lastInFirstOut() {
+	fmt.Println("Counting")
+
+	// Deferred function calls are pushed onto a stack. When a function returns
+	// its deferred calls are executed in last-in-first-out order.
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+
+	fmt.Println("lastInFirstOut() returned")
 }
